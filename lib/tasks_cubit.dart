c552aa15c3c26a9508807b4dc061b8000b687194
Simon Wilmots <simon.wilmots1@gmail.com>
2021-11-17 20:09:42 +0100

    State changing functions

--- lib/tasks_cubit.dart
+++ lib/tasks_cubit.dart
@@ -1,14 +1,22 @@
 // TODO Extend from Cubit<List<String>> and have add/remove emit new states
 // Tip: provide an initial test list in the required super() call to see if your screen is working
-class TasksCubit {
-  TasksCubit() : super();
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TasksCubit extends Cubit<List<String>> {
+  TasksCubit() : super(['Init']);
 
   void add(String task) async {
     // TODO Emit a new state based on the current `state`
     // Tip: make sure to create a new list object, not just mutate the existing list
+    var newState = state;
+    newState.add(task);
+    emit(newState);
   }
 
   void remove(String task) {
     // TODO Emit a new state with the given task removed from the current `state`
+    var newState = state;
+    newState.remove(task);
+    emit(newState);
   }
 }
